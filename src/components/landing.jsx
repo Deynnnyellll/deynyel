4b46ff4871d573379e988698c94e7eb5d732d216
Deynnnyellll <acadsnidanniel04@gmail.com>
2024-08-11 16:53:24 +0800

    changed path

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,4 +1,5 @@
 import { Typer } from "./typer";
+import CV from '/src/assets/docs/CV.pdf'
 
 export const Landing = () => {
 
@@ -8,7 +9,7 @@ export const Landing = () => {
         <div className="h-[85%] md:h-auto flex items-center flex-col justify-end">
             <Typer />
             <button className="text-black rounded-3xl bg-white px-8 py-2 text-sm md:text-lg font-semibold mt-4 md:mt-8 shadow-md shadow-black hover:bg-slate-100 hover:bg-opacity-50 ease-in-out duration-200">
-              <a href="/src/assets/docs/CV.pdf" target="_blank" rel="noopener noreferrer"> Curriculum Vitae </a> 
+              <a href={CV} target="_blank" rel="noopener noreferrer"> Curriculum Vitae </a> 
             </button>
         </div>
     </div>
