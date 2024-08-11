1da817377cdcd41bee28395cdc3bb89292090e98
Deynnnyellll <acadsnidanniel04@gmail.com>
2024-08-11 17:18:48 +0800

    fixed file path, positioning of typer

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,12 +1,12 @@
 import { Typer } from "./typer";
-import CV from '/src/assets/docs/CV.pdf';
+import CV from '/src/assets/docs/CV2.pdf';
 
 export const Landing = () => {
 
 
   return (
     <div id="home" className="h-[100vh] md:h-[115vh] bg-image flex items-center flex-col md:flex-row md:justify-end">
-        <div className="h-[85%] md:h-auto flex items-center flex-col justify-end">
+        <div className="h-[75%] md:h-auto flex items-center flex-col justify-end">
             <Typer />
             <button className="text-black rounded-3xl bg-white px-8 py-2 text-sm md:text-lg font-semibold mt-4 md:mt-8 shadow-md shadow-black hover:bg-slate-100 hover:bg-opacity-50 ease-in-out duration-200">
               <a href={CV} target="_blank" rel="noopener noreferrer"> Curriculum Vitae </a> 
--- src/components/typer.jsx
+++ src/components/typer.jsx
@@ -5,11 +5,11 @@ export const Typer = () => {
       <div className='flex flex-col items-center me-0 md:me-20'>
           <h1 className='text-[40px] md:text-[80px] font-bold md:font-normal'> Hi, I am Danniel </h1>
           <h4 className='text-3xl md:text-5xl'>
-               &#123; <span className='text-gray-400 font-bold text-3xl md:text-5xl'>
+               <span className='text-gray-400 font-bold text-3xl md:text-5xl'>
                       <Typewriter words={['Web Developer', 'Python Developer', 'AI/ML Engineer']} loop={true} cursor typeSpeed={100}>
                           {({ currentWord }) => <>{currentWord}</>}
                       </Typewriter>
-                </span> &#125;
+                </span>
           </h4>
       </div>
   )
