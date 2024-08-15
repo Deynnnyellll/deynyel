ce97d1cf73415de1d87515cd7c017c164ecb3d3f
Deynnnyellll <acadsnidanniel04@gmail.com>
2024-08-15 12:56:44 +0800

    changed color

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -8,15 +8,15 @@ const Skills = () => {
     <div ref={ref} className={isVisible ? 
                               "mt-20 md:w-[60%] w-[85%] md:h-[400px] relative z-50 left-[50%] translate-x-[-50%] ease-in-out duration-500" : "mt-20 md:w-[60%] w-[85%] md:h-[400px] relative z-50 left-[-50%] ease-in-out duration-500"
                             }>
-        <div className="w-full h-full bg-blue-300 text-black py-10 px-6 rounded-xl flex flex-col items-center justify-between shadow-[#864AF9] shadow-lg"> 
+        <div className="w-full h-full bg-[#864AF9] text-white py-10 px-6 rounded-xl flex flex-col items-center justify-between shadow-[#372e49] shadow-md"> 
           <h1 className="text-4xl md:text-5xl font-bold"> Expertise </h1>
           <div className="h-full w-full flex flex-col md:flex-row items-center md:mt-10 md:items-start justify-between">
             {
               skills.map(skill => (
                 <div key={skill.id} className="mt-8 md:mt-0 flex flex-col items-center justify-center w-full md:w-[35%] text-center">
-                  <div className=" text-[50pt] md:text-[60pt] md:mb-5"> {skill.icon} </div>
+                  <div className=" text-[50pt] md:text-[75pt] md:mb-5"> {skill.icon} </div>
                   <h1 className="text-lg md:text-2xl font-bold"> {skill.name} </h1>
-                  <p className="text-[8pt] md:text-[10pt] text-justify md:w-[90%]"> {skill.description} </p>
+                  <p className="md:mt-1 text-[8pt] md:text-[10pt] text-justify md:w-[90%]"> {skill.description} </p>
                 </div>
               ))
             }
